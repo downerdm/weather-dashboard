8f0adbdbd177231bbd26b39d2bce143c720a81ae
Dennis Downer <downerdm@gmail.com>
2022-02-09 23:36:11 -0500

    Updated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,60 @@
 var citySearchEL=document.querySelector("#city");
 var pastSearchEL=document.querySelector("#pastSearchButton");
 var apiKey = "2ca49c5afa747846009dbc9516263b08";
-var city;
+var temp=$("#temp");
+var wind=$("#wind");
+var humidity=$("#humidity");
+var uvi=$("#uvi");
 
-document.getElementById("search").addEventListener("click", function(){
-    window.location = document.getElementById('city').value;
-    console.log(citySearchEL=document.querySelector("#city"));
-    console.log="search button clicked"
+document.getElementById("search").addEventListener("click", function(event){
+    event.preventDefault();
+    var citySelect = document.getElementById('city').value;
+    console.log(citySelect);
+    console.log("search button clicked");
 });
 
-getCurrentWeather = function(city) {
+document.addEventListener("DOMContentLoaded", function(event) { 
+   getLatLong("Atlanta");
+  });
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  function getLatLong (city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + apiKey;
+    fetch(queryURL)
+    .then(function(response){
+        
+        
+        response.json().then(function(data) {
+        console.log(data);
+        var lat=data.coord.lat;
+        var long=data.coord.lon;  
+        getWeatherData(lat,long);
+        })
+    })
+}
+
+function getWeatherData(lat, long) {
+var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon="+ long+"&exclude=hourly,minutely&units=imperial&appid="+ apiKey;
 
 fetch(queryURL)
 .then(function(response){
     response.json().then(function(data) {
-        showWeather(data, city);
+        console.log(data);
+    $(date).html(currentDate);
+    $(temp).html(data.current.temp); 
+    $(wind).html(data.current.wind_speed); 
+    $(humidity).html(data.current.humidity); 
+    $(uvi).html(data.current.uvi); 
+    for (i=0; i<5; i++){
+        var dayData = data.daily[i];
+        var currentDate = new Date(dayData.dt*1000).toLocaleDateString();
+        var icon = "https://openweathermap.org/img/wn/" + dayData.weather[0].icon + ".png";
+        $("#date"+i).html(currentDate);
+        $("#icon"+i).html("<img src=" + icon + ">");
+        $("#temp"+i).html(dayData.temp.day);
+        $("#wind"+i).html(dayData.wind_speed) + "mph";
+        $("#humidity"+i).html(dayData.humidity);
+
+    }
+    
     })
-})
-}
-
-
-
+})}
